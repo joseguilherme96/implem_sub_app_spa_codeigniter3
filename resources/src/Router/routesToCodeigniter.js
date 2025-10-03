e82d46a94b92cda5c2239ba1987cc9079a3ec7db
José GUilherme <jose.guilherme96@outlook.com.br>
2025-10-03 11:32:59 -0300

    inclui rota para acesso a view CadastroOrcamento.vue no codeigniter

--- resources/src/Router/routesToCodeigniter.js
+++ resources/src/Router/routesToCodeigniter.js
@@ -1,7 +1,9 @@
+import CadastroOrcamento from '../Pages/CadastroOrcamento.vue'
 import { Relatorio } from '../Pages/Relatorio'
 
 const routesToCodeigniter = {
     '/relatorio': Relatorio,
+    '/cadastro-orcamento': CadastroOrcamento
 }
 
 export default routesToCodeigniter
